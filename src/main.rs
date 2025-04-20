aa72e6c814d1ec154ded322fe2a66cd1229de99d
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-04-20 22:14:36 +0200

    update template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![no_std]
+#![no_main]
+
+use wut;
+use wut::prelude::*;
+use wut::time::Duration;
+
+#[wut::main(Console)]
 fn main() {
-    println!("Hello, world!");
+    while wut::process::running() {
+        println!("{}", wut::time::DateTime::now());
+        wut::thread::sleep(Duration::from_secs(1));
+    }
 }
